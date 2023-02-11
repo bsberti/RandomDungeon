e251837845aa8eeb186c6f7af324a92fa43b9ddf
Diego Bastos <bastosdiegol@gmail.com>
2023-02-11 00:49:37 -0500

    JSON being read succesfully

--- JSONProject/main.cpp
+++ JSONProject/main.cpp
@@ -2,9 +2,31 @@
 #include "rapidjson/writer.h"
 #include "rapidjson/stringbuffer.h"
 
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
 int main(int argc, char* argv[]) {
 
+	std::ifstream file("entityLoader.json");
+	std::stringstream buffer;
+	buffer << file.rdbuf();
+	std::string content = buffer.str();
 
+	rapidjson::Document doc;
+	doc.Parse(content.c_str());
+
+	const rapidjson::Value& className = doc["className"];
+	const rapidjson::Value& attributes = doc["attributes"];
+	std::cout << "Classname: " << className.GetString() << std::endl;
+
+	for (rapidjson::SizeType i = 0; i < attributes.Size(); i++) {
+		const rapidjson::Value& attribute = attributes[i];
+		std::string attrName = attribute["name"].GetString();
+		int attrValue = attribute["value"].GetInt();
+		
+		std::cout << "Attribute: " << attrName << " | Value: " << attrValue << std::endl;
+	}
 
 	return 0;
 }
